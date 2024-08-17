18dfd0451244a8026682bf462d7b5ad2e35032f3
Sujeet B <12060698+sujeet-bhalerao@users.noreply.github.com>
2024-08-17 17:52:54 -0500

    changes with working webapp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
-    websiteName: "uiucmcs.org",
-    production: true,
+    websiteName: "uiuchub.com", 
+    production: true, 
     firebase: {
-        apiKey: "AIzaSyDJNQ2n0BIfccr5-QQ80qVx0A6hYYBCo4Y",
-        authDomain: "uiuc-mcs-org.firebaseapp.com",
-        databaseURL: "https://uiuc-mcs-org-default-rtdb.firebaseio.com",
-        projectId: "uiuc-mcs-org",
-        storageBucket: "uiuc-mcs-org.appspot.com",
-        messagingSenderId: "869464868463",
-        appId: "1:869464868463:web:fc955a3ec3cb5807a16f9a",
-        measurementId: "G-E8VEJQTBME"
+        apiKey: "AIzaSyAklMrJioaJPCkU6pT8W7Yq0IfsQ0h3o88",
+        authDomain: "uiuc-course-hub.firebaseapp.com",
+        databaseURL: "https://uiuc-course-hub-default-rtdb.firebaseio.com",
+        projectId: "uiuc-course-hub",
+        storageBucket: "uiuc-course-hub.appspot.com",
+        messagingSenderId: "74220647977",
+        appId: "1:74220647977:web:ab544bacd4aa400960ce38",
+        measurementId: "G-PWWS2YBZE0"
     }
 };
